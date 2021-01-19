3f08060aaa24e9536369b0d4e96b8ba9dcbbbb01
Patrick Coxall <mr.coxall@mths.ca>
2021-01-19 10:57:19 -0500

    Create Temp.swift

---
+++ Temp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import PlaygroundSupport
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .font(.headline)
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
